c7c851cd7bf051d1e53e5e00fb88b0fccc88a16e
Jean THOMAS <git0@pub.jeanthomas.me>
2018-01-20 21:39:23 +0100

    Fixing copyright date

--- src/stlink.h
+++ src/stlink.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2017 Jean THOMAS.
+  Copyright (c) 2018 Jean THOMAS.
   
   Permission is hereby granted, free of charge, to any person obtaining
   a copy of this software and associated documentation files (the "Software"),
